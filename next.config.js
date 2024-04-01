a4c211ab3ae634924ffd993974ca6d316078e392
Ваше Имя <.email@example.com>
2024-04-02 01:45:22 +0300

    change env paths

--- next.config.js
+++ next.config.js
@@ -18,11 +18,11 @@ const nextConfig = {
 		return [
 			{
 				source: '/api/:path*',
-				destination: 'http://localhost:3000/api/:path*'
+				destination: `${process.env.NEXT_PUBLIC_APP_URL}/:path*`
 			},
 			{
 				source: '/uploads/:path*',
-				destination: 'http://localhost:3000/uploads/:path*'
+				destination: `${process.env.NEXT_PUBLIC_APP_URL_UPLOADS}/:path*`
 			}
 		]
 	}
